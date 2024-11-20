00fc1395a15e16fc76f68a278ffe3fb8723c9800
Orekov Anton <orekhovant@icloud.com>
2024-11-20 20:51:55 +0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Антон Орехов on 14.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
