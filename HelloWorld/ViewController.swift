ec05a8facec2fd495e8ef0710d707cd4ce52cdab
Orekov Anton <orekhovant@icloud.com>
2024-11-23 03:39:36 +0500

    Setup Main

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    
+    @IBOutlet var showGreetingButton: UIButton!
+    
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showGreetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        
+        showGreetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+    }
+    
 }
+
+
 
